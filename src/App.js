8b16b71657993071279e8aabeed3d04802f62aee
lijoyoommen007 <108475172+lijoyoommen007@users.noreply.github.com>
2024-09-26 23:03:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // App.js
-import React, { useEffect } from "react";
+import React from "react";
 // import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 // import ChatApp from "./pages/chatbot";
-import RepresentativePanel from "./pages/representative";
+// import RepresentativePanel from "./pages/representative";
 import Testone from "./pages/testone.jsx";
 
 
